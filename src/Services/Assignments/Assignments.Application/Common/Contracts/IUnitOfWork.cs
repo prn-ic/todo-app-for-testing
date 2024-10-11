00ccaf56f7c5c1bc69f46f11b2fcea4ff64527b1
prn-ic <domarlaid2@mail.ru>
2024-10-11 19:22:53 +0500

    some changes rider test

--- src/Services/Assignments/Assignments.Application/Common/Contracts/IUnitOfWork.cs
+++ src/Services/Assignments/Assignments.Application/Common/Contracts/IUnitOfWork.cs
@@ -8,11 +8,13 @@ public interface IUnitOfWork
     Task<IEnumerable<T>> QueryAsync<T>(
         Expression<Func<T, bool>> expression = null!,
         CancellationToken cancellationToken = default
-    ) where T : class, IBaseEntity;
+    )
+        where T : class, IBaseEntity;
     Task<T?> QuerySingleAsync<T>(
         Expression<Func<T, bool>> expression,
         CancellationToken cancellationToken = default
-    ) where T : class, IBaseEntity;
+    )
+        where T : class, IBaseEntity;
     Task<int> SaveChangesAsync(
         List<IBaseEntity> changedEntities,
         CancellationToken cancellationToken = default
--- src/Services/Assignments/Assignments.Application/Common/Specifications/EmptySpecification.cs
+++ src/Services/Assignments/Assignments.Application/Common/Specifications/EmptySpecification.cs
@@ -1,3 +1,4 @@
+
 using System.Linq.Expressions;
 
 namespace Assignments.Application.Common.Specifications;
